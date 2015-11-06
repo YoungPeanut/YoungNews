1a3332765102b39526dc35bac5bc83530befdeb4
chenshaosina <chenshaosina@chenshaosinadeMacBook-Pro.local>
2015-11-06 18:57:48 +0800

    theme

--- youngnews/src/main/java/info/ipeanut/youngnews/ui/fragment/NewsPageFragment.java
+++ youngnews/src/main/java/info/ipeanut/youngnews/ui/fragment/NewsPageFragment.java
@@ -1,9 +1,24 @@
 package info.ipeanut.youngnews.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import info.ipeanut.youngnews.R;
 import info.ipeanut.youngnews.ui.base.BaseFragment;
 
 /**
  * Created by chenshao on 15/11/5.
  */
 public class NewsPageFragment extends BaseFragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frag_newspage,null);
+        return view;
+    }
 }
